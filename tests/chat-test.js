01b81d2c8ff97faae965f977d0e44ff2892745ab
Carl <carlcapozza@gmail.com>
2015-06-18 10:02:58 -0400

    Removing the Jest sample test as it wasn't needed.

--- tests/chat-test.js
+++
@@ -1,7 +0,0 @@
-require('./testdom')('<html><body></body></html>');
-describe('sum', function() {
- it('adds 1 + 2 to equal 3', function() {
-   var sum = require('../sum');
-   expect(sum(1, 2)).toBe(3);
- });
-});
